649d721b079497d509bbcd8c35d6c3fc0f13d877
eMobilis <chrispycharmprojects@gmail.com>
2023-04-19 09:45:04 +0300

    first commit

---
+++ app/src/main/java/com/example/authe_sql/loginactivity.kt
@@ -0,0 +1,11 @@
+package com.example.authe_sql
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loginactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loginactivity)
+    }
+}
